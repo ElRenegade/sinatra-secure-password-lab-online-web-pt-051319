55e8df791d6b6de3b759dc1e94589237ccb5909d
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-23 02:40:52 +0000

    Automatically backed up by Learn

--- db/migrate/20190823023451_users.rb
+++ db/migrate/20190823023451_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.text :username
-      t.text :password
+      t.text :password_digest
     end
   end
 end
